bde176bc06df9d01567186720c6a2dac72753872
ashitaka1963 <ashitaka1963@gmail.com>
2023-08-25 21:09:42 +0900

    feat: OpenAPI(Swagger)初期セットアップ

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // ルートエンドポイントの設定
 app.get("/", (req, res) => {
   res.send("Hello Express!");
