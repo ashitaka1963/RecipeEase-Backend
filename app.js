c9083aad4f4fbebcbbc4b06e375d1d4080de52cb
ashitaka1963 <ashitaka1963@gmail.com>
2023-08-26 19:08:35 +0900

    feat: MongoDB初期セットアップ

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+
+// MongoDBに接続
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Recipease", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
